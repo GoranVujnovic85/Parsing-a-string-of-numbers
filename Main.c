f78c278899918a32b7b6d181c7ede52b27b14d36
Goran <goranvujnovic85@gmail.com>
2023-10-10 12:29:31 +0200

    third commit with function ERRNO

--- Main.c
+++ Main.c
@@ -8,11 +8,11 @@ Description :
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <errno.h>
 
 int main()
 {
-	char string[]= "-1+1-3+3+1000";
+	char string[]= "-1+1-3+3+1000+100000000000000000000000000";
 
 	char *cursor = string;
 	int sum = 0;
@@ -22,6 +22,14 @@ int main()
 	{
 		int x = strtol(cursor, &cursor, 10);
 
+		/* ERRNO se koristi za detekciju gresaka, ako se postavi na ERANGE,
+		 zanci da izlazi van domasaja u nasem slucaju int. Dolazi do underflow ili overflow.
+		 Cesto se koristi kod funkcija atoi, strtol */
+		if(errno == ERANGE)
+		{
+			printf("Number is too big for store!\n");
+
+		}
 		if(flag == 1)
 		{
 			sum += x;
